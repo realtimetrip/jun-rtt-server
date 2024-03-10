61710e98fc9f92237e214a9538dde422f1b228fc
bbangjun <lucid9dawn@gmail.com>
2024-03-10 17:35:13 +0900

    docs(.gitignore): git ignore 초기 설정

---
+++ src/main/java/com/bbangjun/realtimetrip/RealtimetripApplication.java
@@ -0,0 +1,13 @@
+package com.bbangjun.realtimetrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimetripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimetripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbangjun/realtimetrip/RealtimetripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbangjun.realtimetrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimetripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
