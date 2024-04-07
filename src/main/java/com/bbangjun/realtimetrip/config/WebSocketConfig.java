2943b053ffc9dfe8622709200e8d4f37d799b510
bbangjun <lucid9dawn@gmail.com>
2024-04-07 17:33:25 +0900

    feat(WebSocketConfig): RabbitMQ 연동을 위한 config 수정

--- src/main/java/com/bbangjun/realtimetrip/config/WebSocketConfig.java
+++ src/main/java/com/bbangjun/realtimetrip/config/WebSocketConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -16,7 +17,22 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config){
-        config.enableSimpleBroker("/sub");
+//        // websocket + stomp
+//        config.enableSimpleBroker("/sub");
+//        config.setApplicationDestinationPrefixes("/pub");
+
+        // RabbitMQ 연동
+        // 메시지 구독 url
+        config.enableStompBrokerRelay("/exchange")
+                .setClientLogin(rabbitUser)
+                .setClientPasscode(rabbitPwd)
+                .setSystemLogin(rabbitUser)
+                .setSystemPasscode(rabbitPwd)
+                .setRelayHost(rabbitHost)
+                .setRelayPort(rabbitPort)
+                .setVirtualHost(rabbitVHost);
+        // 메시지 발행 url
+        config.setPathMatcher(new AntPathMatcher("."));
         config.setApplicationDestinationPrefixes("/pub");
     }
 
