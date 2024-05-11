2b30feb829b5f903f321fb4d3b13791d773c3a93
bbangjun <lucid9dawn@gmail.com>
2024-05-11 22:08:33 +0900

    feat: webSocketConfig 설정 (STOMP + SimpleBroker)

--- src/main/java/com/bbangjun/realtimetrip/config/WebSocketConfig.java
+++ src/main/java/com/bbangjun/realtimetrip/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.bbangjun.realtimetrip.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.AntPathMatcher;
@@ -11,33 +12,23 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Slf4j
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker // 웹소켓 활성화
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config){
-//        // websocket + stomp
-//        config.enableSimpleBroker("/sub");
-//        config.setApplicationDestinationPrefixes("/pub");
-
-        // RabbitMQ 연동
-        // 메시지 구독 url
-        config.enableStompBrokerRelay("/exchange")
-                .setClientLogin(rabbitUser)
-                .setClientPasscode(rabbitPwd)
-                .setSystemLogin(rabbitUser)
-                .setSystemPasscode(rabbitPwd)
-                .setRelayHost(rabbitHost)
-                .setRelayPort(rabbitPort)
-                .setVirtualHost(rabbitVHost);
-        // 메시지 발행 url
-        config.setPathMatcher(new AntPathMatcher("."));
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // 메시지 브로커의 Prefix를 등록
+        // 클라이언트는 topic 구독 시 /sub 경로로 요청해야 함.
+        config.enableSimpleBroker("/sub");
+        //  클라이언트에서 메시지 발행 시 해당 메시지 매핑에 대한 접두사로 사용
         config.setApplicationDestinationPrefixes("/pub");
     }
 
-    public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*").withSockJS();
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // 클라이언트에서 메시지 발행 시 해당 메시지 매핑에 대한 접두사로 사용됨.
+        // 웹소켓 연결에 필요한 Endpoint를 지정: /ws-stomp
+        // setAllowedOriginPatterns의 인자를 *로 설정하여 모든 출처에 대한 CORS 설정
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS();
     }
 }
